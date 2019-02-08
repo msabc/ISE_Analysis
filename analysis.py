e7a2e921371eb50637dececea37e4bdeff607a61
Matija Sabljic <masabljic@infobip.com>
2019-02-08 19:45:39 +0100

    cleaned up missing values (zeroes)

--- analysis.py
+++ analysis.py
@@ -13,3 +13,43 @@ print("Columns: " + str(dfshape[1]))
 print()
 
 print(mainDataFrame.info())
+
+#print(mainDataFrame.columns[0] > 100)
+print()
+
+# replacing all 0's with 'missing'
+mainDataFrame = mainDataFrame.replace({
+        'NewYork_SP500': {
+            0:  "missing",
+        },
+         'DAX': {
+            0:  "missing",
+        },
+         'FTSE': {
+            0:  "missing",
+        },
+         'NIKKEI': {
+            0:  "missing",
+        },
+         'BOVESPA': {
+            0:  "missing",
+        },
+          'EU': {
+            0:  "missing",
+        },
+          'EM': {
+            0:  "missing",
+        }
+    })
+
+# not including the first two columns and the last column, since they dont have any missing values
+mainDataFrame = mainDataFrame[(mainDataFrame['NewYork_SP500'] != 'missing') & (mainDataFrame['DAX'] != 'missing') & (mainDataFrame['FTSE'] != 'missing') & (mainDataFrame['NIKKEI'] != 'missing') & (mainDataFrame['BOVESPA'] != 'missing') & (mainDataFrame['EU'] != 'missing')]
+
+print("State after removing rows with missing values: ")
+print(mainDataFrame.shape)
+
+# histogram
+mainDataFrame.hist(figsize=(10,7))
+
+print(mainDataFrame.describe())
+
